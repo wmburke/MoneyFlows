345f72c265229a0ca3f0cbdcc62ff1416c5ecdce
w-m-burke <wmb@waynemosesburke.com>
2014-07-15 14:38:39 -0400

    Basics are in place - you can add flows and see them all add up right...

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@
 	var db = new PouchDB('MoneyFlows'),
 	remoteCouch,
 	ENTER_KEY = 13,
+	ESC_KEY = 27,
+	TAB_KEY = 9,
 	
 	// these connect variables to data entry fields on the page
 	dom = {
@@ -18,6 +20,10 @@
 		isTaxable: document.getElementById('isTaxable'),
 		isLoan: document.getElementById('isLoan'),
 		notes: document.getElementById('notes'),
+		
+		totalMaybe: document.getElementById('totalMaybe'),
+		totalReal: document.getElementById('totalReal'),
+		dateReal: document.getElementById('dateReal'),
 		
 		newFlow: document.getElementById('newFlow'),
 		sync: document.getElementById('syncWrapper')
@@ -32,6 +38,7 @@
 			
 			// Validate and format dateDue
 			if (flow.dateDue !== "") {
+				
 				date = WMB.checkDate(flow.dateDue);
 				if (date === false) {
 					alert('Your Due Date is bad.\n\nPlease try something like the following: 4/30 or 4/30/13 to specify a year. You can also use - or . as separators.');
@@ -50,8 +57,21 @@
 				flow.datePaid = date;
 			}
 			
+			// Make sure the additional ...Year items don't make their way back to the database
+			if (flow.dateDueYear) {
+				delete flow.dateDueYear;
+			}
+			if (flow.datePaidYear) {
+				delete flow.datePaidYear;
+			}
+						
 			// Format amount
 			flow.amount = flow.amount * 1000;
+			
+			// Check for notes
+			if (flow.notes === "") {
+				flow.notes = "No notes for this flow.";
+			}
 			
 			return flow;
 
@@ -80,17 +100,21 @@
 			});
 		},
 		
-		formatOutput: function(flow) {
+		prepFromDB: function(flow) {
+		// take the info for a flow from the DB and make it ready for printing
+
 			var date;
 
 			// format the dates
 			if (flow.dateDue !== "") {
 				date = new Date(flow.dateDue);
 				flow.dateDue = date.getMonth() + 1 + '/' + date.getDate();
+				flow.dateDueYear = '/' + (date.getYear() + 1900);
 			}
 			if (flow.datePaid !== "") {
 				date = new Date(flow.datePaid);
 				flow.datePaid = date.getMonth() + 1 + '/' + date.getDate();
+				flow.datePaidYear = '/' + (date.getYear() + 1900);
 			}
 			
 			// format the amounts
@@ -112,44 +136,86 @@
 	flows = {
 	// for showing and editing the flows list
 	
-		makeCheckbox: function(flow, name, listen) {
+		// variables to track against the flows
+		totalMoneyMaybe : 0,
+		totalMoneyReal: 0,
+		lastDateReal: 0,
+		tabIndex: 100,
+		
+		makeField: function(flow, name) {
+			var label = document.createElement('div');
+			label.className = 'col_' + name;
+			label.appendChild(document.createTextNode(flow[name]));
+			label.title = flow[name];
+			label.contentEditable = "true";
+			label.setAttribute("tabindex", this.tabIndex);
+			this.tabIndex += 1;
+			if (name.lastIndexOf("date", 0) === 0 && flow[name] !== "") {
+				label.title += flow[name + "Year"];
+			}
+			label.addEventListener('keypress', this.keyPressed.bind(this, flow, name));
+			return label;
+		},
+		
+		makeCheckbox: function(flow, name, listener) {
 		// create the checkbox object
-		// if listen is true, add the event listener
 			var checkbox = document.createElement('input');
 			checkbox.className = 'col_' + name;
 			checkbox.type = 'checkbox';
 			if (flow[name] === true) {
 				checkbox.checked = flow[name]; 
 			}
-			if (listen === true) {
+			checkbox.setAttribute("tabindex", this.tabIndex);
+			this.tabIndex += 1;
+			if (listener === "changed") {
 				checkbox.addEventListener('change', this.checkboxChanged.bind(name, this, flow));
+			} else if (listener === "showPopup") {
+				checkbox.addEventListener('change', this.checkboxShowPopup.bind(name, this, flow));
 			}
 			return checkbox;
 		},
 		
-		makeLabel: function(flow, name) {
-			var label = document.createElement('label');
-			label.className = 'col_' + name;
-			label.appendChild(document.createTextNode(flow[name]));
-			label.title = flow[name];
-			label.addEventListener('dblclick', this.dblClicked.bind(name, this, flow));
-			return label;
-		},
-		
-		makeButton: function(flow, name) {
+		makeDestroy: function(flow, name) {
 			var button = document.createElement('name');
 			button.className = 'destroy';
 			button.addEventListener( 'click', this.deleteButtonPressed.bind(this, flow));
 			return button;
 		},
 		
-		makeEditable: function(flow, name) {
-			var editable = document.createElement('input');
-			editable.id = 'input_' + flow._id + '_' + name;
-			editable.className = 'col_' + name;
-			editable.value = flow[name];
-			editable.addEventListener('keypress', this.keyPressed.bind(this, flow, name));
-			return editable;
+		make_spanPopup: function(harborFrom, isTaxable, isLoan, notes) {
+		// make the spanPopup for the flows
+			
+			// create the elements
+			var	spanPopup = document.createElement('span'),
+			div_harborFrom = document.createElement('div'),
+			span_harborFrom = document.createElement('span'),
+			div_isTaxable = document.createElement('div'),
+			span_isTaxable = document.createElement('span'),
+			div_isLoan = document.createElement('div'),
+			span_isLoan = document.createElement('span');
+			
+			// add classes
+			spanPopup.className = "invisible flowHiddenInfo";
+			
+			// make the divs
+			div_harborFrom.innerHTML = 'From ';
+			div_harborFrom.appendChild(harborFrom);
+			div_isTaxable.innerHTML = "Is this flow taxable? ";
+			div_isTaxable.appendChild(isTaxable);
+			div_isLoan.innerHTML = "Is this flow a loan? ";
+			div_isLoan.appendChild(isLoan);
+			
+			// add it up
+			spanPopup = this.appendChildren(spanPopup, [div_harborFrom, div_isTaxable, div_isLoan, notes]);
+			spanPopup.contentEditable = "false";
+			return spanPopup;
+		},
+		
+		appendChildren: function(object, appendages) {
+			appendages.forEach(function(child) {
+				object.appendChild(child);
+			});
+			return object;
 		},
 		
 		checkboxChanged: function(name, flow, event) {
@@ -160,32 +226,41 @@
 			db.put(flow);
 		},
 		
-		dblClicked: function(name, flow) {
-		// User has double clicked a field, display an input so they can edit it
-			var div = document.getElementById('div_' + flow._id),
-			inputEdit = document.getElementById('input_' + flow._id + '_' + this);
-			div.className = 'editing';
-			inputEdit.focus();
+		checkboxShowPopup: function(name, flow, event) {
+		// show/hide flowHiddenInfo
+			if (event.target.checked === true) {
+				event.target.parentNode.childNodes[8].className = "flowHiddenInfo";
+			} else {
+				event.target.parentNode.childNodes[8].className = "invisible flowHiddenInfo";
+			}
 		},
-
+		
 		keyPressed: function(flow, name, event) {
 		// If they press enter while editing an entry, save the row
 			if (event.keyCode === ENTER_KEY) {
-				var fields = event.target.parentNode.childNodes;
-				flow.dateDue = fields[0].value.trim();
-				flow.harborTo = fields[1].value.trim();
-				flow.amount = fields[2].value.trim();
-				flow.isScheduled = fields[3].checked;
-				flow.datePaid = fields[4].value.trim();
-				flow.harborFrom = fields[5].value.trim();
-				flow.isTaxable = fields[6].checked;
-				flow.isLoan = fields[7].checked;
-				flow.notes = fields[8].value.trim();
+				var fields;
+				// if name equals any of these:
+				if (["harborFrom", "isTaxable", "isLoan", "notes"].indexOf(name) !== -1) {
+					fields = event.target.parentNode.parentNode.parentNode.childNodes;
+				} else {
+					fields = event.target.parentNode.childNodes;
+				}
+				flow.dateDue = fields[0].textContent.trim();
+				flow.harborTo = fields[1].textContent.trim();
+				flow.amount = fields[2].textContent.trim();
+				flow.datePaid = fields[3].textContent.trim();
+				flow.isScheduled = fields[4].checked;
+				flow.harborFrom = fields[8].childNodes[0].childNodes[1].textContent.trim();
+				flow.isTaxable = fields[8].childNodes[1].childNodes[1].checked;
+				flow.isLoan = fields[8].childNodes[2].childNodes[1].checked;
+				flow.notes = fields[8].childNodes[3].textContent.trim();
 				
 				mainPage.prepForDB(flow);
 			
 				db.put(flow);
-			}
+			} //else if (event.keyCode === ESC_KEY) {
+//				this.doIt();
+//			}
 		},
 
 		deleteButtonPressed: function(flow) {
@@ -193,78 +268,54 @@
 			db.remove(flow);
 		},
 
-		createDiv: function(flow) {
+		makeRow: function(flow) {
 		// Create a div to display a flow.
 		
 			// fix the display
-			mainPage.formatOutput(flow);
+			mainPage.prepFromDB(flow);
 		
 			// create all the components of the div
 			
 			// these are the overarching elements that contain the others
-			var	div_ = document.createElement('div'),
-			divEdit = document.createElement('div'),
-			divView = document.createElement('div'),
-			spanHidden = document.createElement('span'),
+			var	divRow = document.createElement('div'),
 			
 			// these are all the elements to show 
-			dateDue = this.makeLabel(flow, 'dateDue'),
-			harborTo = this.makeLabel(flow, 'harborTo'),
-			amount = this.makeLabel(flow, 'amount'),
-			isScheduled = this.makeCheckbox(flow, 'isScheduled', true),
-			datePaid = this.makeLabel(flow, 'datePaid'),
-			harborFrom = this.makeLabel(flow, 'harborFrom'),
-			isTaxable = this.makeCheckbox(flow, 'isTaxable', true),
-			isLoan = this.makeCheckbox(flow, 'isLoan', true),
-			notes = this.makeLabel(flow, 'notes'),
-			deleteLink = this.makeButton(flow, 'deleteLink'),
+			dateDue = this.makeField(flow, 'dateDue'),
+			harborTo = this.makeField(flow, 'harborTo'),
+			amount = this.makeField(flow, 'amount'),
+			datePaid = this.makeField(flow, 'datePaid'),
+			isScheduled = this.makeCheckbox(flow, 'isScheduled', 'changed'),
+			showPopup = this.makeCheckbox(flow, 'showPopup', 'showPopup'),
+			harborFrom = this.makeField(flow, 'harborFrom'),
+			isTaxable = this.makeCheckbox(flow, 'isTaxable', 'changed'),
+			isLoan = this.makeCheckbox(flow, 'isLoan', 'changed'),
+			notes = this.makeField(flow, 'notes'),
+			spanPopup = this.make_spanPopup(harborFrom, isTaxable, isLoan, notes),
+			deleteLink = this.makeDestroy(flow, 'deleteLink'),
 
-			// these are all the hidden edit elements
-			edit_dateDue = this.makeEditable(flow, 'dateDue'),
-			edit_harborTo = this.makeEditable(flow, 'harborTo'),
-			edit_amount = this.makeEditable(flow, 'amount'),
-			edit_isScheduled = this.makeCheckbox(flow, 'isScheduled'),
-			edit_datePaid = this.makeEditable(flow, 'datePaid'),
-			edit_harborFrom = this.makeEditable(flow, 'harborFrom'),
-			edit_isTaxable = this.makeCheckbox(flow, 'isTaxable'),
-			edit_isLoan = this.makeCheckbox(flow, 'isLoan'),
-			edit_notes = this.makeEditable(flow, 'notes');
-
-			// put the view elements together in a div
-			divView.className = 'view';
-			divView.appendChild(dateDue);
-			divView.appendChild(harborTo);
-			divView.appendChild(amount);
-			divView.appendChild(isScheduled);
-			divView.appendChild(datePaid);
-			divView.appendChild(spanHidden);
-			spanHidden.className = "invisible";
-			spanHidden.appendChild(harborFrom);
-			spanHidden.appendChild(isTaxable);
-			spanHidden.appendChild(isLoan);
-			spanHidden.appendChild(notes);
-			divView.appendChild(deleteLink);
+			// add the totals up and include them
+			moneyMaybe = document.createElement('div'),
+			moneyReal = document.createElement('div');
+				// add on amounts to the totals
+			this.totalMoneyMaybe += parseFloat(flow.amount);
+			if (flow.datePaid !== "") {
+				this.totalMoneyReal += parseFloat(flow.amount);
+				this.lastDateReal = flow.datePaid;
+				divRow.className += ' reconciled';
+				isScheduled.className += " invisible";
+			}
+			moneyMaybe.appendChild(document.createTextNode(this.totalMoneyMaybe.toFixed(2)));
+			moneyReal.appendChild(document.createTextNode(this.totalMoneyReal.toFixed(2)));
+			moneyMaybe.className = "col_moneyMaybe";
+			moneyReal.className = "col_moneyReal"; 
 			
-			// put the initially hidden edit elements together
-			divEdit.className = 'edit';
-			divEdit.appendChild(edit_dateDue);
-			divEdit.appendChild(edit_harborTo);
-			divEdit.appendChild(edit_amount);
-			divEdit.appendChild(edit_isScheduled);
-			divEdit.appendChild(edit_datePaid);
-			divEdit.appendChild(edit_harborFrom);
-			divEdit.appendChild(edit_isTaxable);
-			divEdit.appendChild(edit_isLoan);
-			divEdit.appendChild(edit_notes);
-
 			// put it all together
-			div_.id = 'div_' + flow._id;
-			div_.appendChild(divView);
-			div_.appendChild(divEdit);
+			divRow.id = 'div_' + flow._id;
+			divRow = this.appendChildren(divRow, [dateDue, harborTo, amount, datePaid, isScheduled, showPopup, moneyMaybe, moneyReal, spanPopup, deleteLink]);
 
 			// check if the checkboxes should be checked
 			if (flow.isScheduled) {
-				div_.className += 'isScheduled';
+				divRow.className += ' isScheduled';
 				isScheduled.checked = true;
 			}
 			if (flow.isTaxable) {
@@ -274,21 +325,34 @@
 				isLoan.checked = true;
 			}
 
-			return div_;
+			return divRow;
 		},
 
 		redraw: function(flowsToShow) {
 		    var div = document.getElementById('flows');
 			div.innerHTML = '';
+			this.totalMoneyMaybe = 0;
+			this.totalMoneyReal = 0;
+			this.tabIndex = 100;
 			flowsToShow.forEach(function(flowFromDB) {
 				if(flowFromDB.doc._id !== "options") {
-					div.appendChild(flows.createDiv(flowFromDB.doc));
+					div.appendChild(flows.makeRow(flowFromDB.doc));
 				}
 			});
+			dom.totalMaybe.innerHTML = this.totalMoneyMaybe.toFixed(2); 
+			dom.dateReal.innerHTML = this.lastDateReal; 
+			dom.totalReal.innerHTML = this.totalMoneyReal.toFixed(2); 
+		},
+		
+		sort: function(a, b, col) {
+			return a.doc[col]-b.doc[col];
 		},
 
 		doIt: function() {
 			db.allDocs({include_docs: true}, function(err, docs) {
+				docs.rows.sort(function(a, b) {
+					return flows.sort(a, b, "dateDue");
+				});
 				flows.redraw(docs.rows);
 			});
 		}
@@ -311,8 +375,8 @@
 		}
 		
 	};
-
-	function newFlowKeyPressHandler( event ) {
+	
+	function saveNewFlow(event) {
 		if (event.keyCode === ENTER_KEY) {
 			mainPage.add(dom);
 			dom.dateDue.value = '';
@@ -325,19 +389,40 @@
 			dom.isLoan.checked = 0;
 			dom.notes.value = '';
 			dom.dateDue.focus();
+		}		
+	}
+	
+	function makeInvisible(event) {
+		if (event.explicitOriginalTarget.parentNode.className === "flowHiddenInfo invisible") {
+			event.stopImmediatePropogation();
+		} else {
+			dom.newFlow.childNodes[7].className = "flowHiddenInfo invisible";
 		}
 	}
 
-	function addEventListeners() {
-		dom.newFlow.addEventListener('keypress', newFlowKeyPressHandler, false);
+	function makeVisible(event) {
+		dom.newFlow.childNodes[7].className = "flowHiddenInfo";
 	}
 
-	// to remove the database
-/*	db.destroy(function(err, info) {
-		if (err) {console.log('Datebase destruction error: ' + err);}
-		console.log("Databse destroyed! " + info);
-	});
-*/
+	function addEventListeners() {
+		dom.newFlow.addEventListener("keypress", saveNewFlow, true);
+		
+		// make newFlow .flowHiddenInfo disappear
+		document.body.addEventListener("mousedown", makeInvisible, true);
+
+		// make newFlow .flowHiddenInfo appear and stick around for input
+/*		dom.dateDue.addEventListener("focus", makeVisible, true);
+		dom.harborTo.addEventListener("focus", makeVisible, true);
+		dom.amount.addEventListener("focus", makeVisible, true);
+		dom.isScheduled.addEventListener("focus", makeVisible, true);
+*/		dom.datePaid.addEventListener("focus", makeVisible, true);
+		dom.harborFrom.addEventListener("focus", makeVisible, true);
+		dom.isTaxable.addEventListener("focus", makeVisible, true);
+		dom.isLoan.addEventListener("focus", makeVisible, true);
+		dom.notes.addEventListener("focus", makeVisible, true);
+		document.getElementById('newFlow').addEventListener("mousedown", makeVisible, true);
+	}
+
 	db.info(function(err, info) {
 		db.changes({
 			since: info.update_seq,
